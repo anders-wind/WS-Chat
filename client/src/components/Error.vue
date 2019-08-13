2960d9860ff228f69c037776859b299b8aca75a5
Anders Wind <awia00@gmail.com>
2019-08-13 16:19:34 +0200

    Supported Chat service

---
+++ client/src/components/Error.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-snackbar v-model="hasError" color="error" multi-line :timeout="timeout">
+    {{ errorMessage }}
+    <v-btn dark @click="dismiss()">Close</v-btn>
+  </v-snackbar>
+</template>
+
+
+<script lang="ts">
+import { Prop, Component, Vue, Watch } from 'vue-property-decorator';
+@Component({})
+export default class Error extends Vue {
+  private timeout: number = 0;
+  get hasError() {
+    return this.$store.state.hasError;
+  }
+  public dismiss() {
+    this.$store.commit('clearError');
+  }
+  get errorMessage() {
+    return this.$store.state.errorMessage;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,16 +1,65 @@
+import io from 'socket.io-client';
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, {ActionContext} from 'vuex';
+import {ActionTree, GetterTree, MutationTree} from 'vuex';
+import {ChatMessage} from './models/ChatMessage';
 
 Vue.use(Vuex);
 
+
+const host =
+    `http://${process.env.VUE_APP_SERVER}:${process.env.VUE_APP_SERVERPORT}/`;
+const debug = process.env.environment !== 'production';
+
+export const socket = io.connect(host);
+
+// Store specifics
+export class State {
+  public user: string = '';
+  public chatMessages: ChatMessage[] = [];
+  public errorMessage: string = '';
+  public hasError: boolean = false;
+}
+
+const getterTree: GetterTree<State, any> = {};
+
+const mutationTree: MutationTree<State> = {
+  setUser(state: State, newName: string) {
+    state.user = newName;
+  },
+  addMessage(state: State, newMessage: ChatMessage) {
+    state.chatMessages.push(newMessage);
+  },
+  setError(state: State, errorMessage: string) {
+    state.errorMessage = errorMessage;
+    state.hasError = true;
+  },
+  clearError(state: State) {
+    state.errorMessage = '';
+    state.hasError = false;
+  },
+};
+
+const actionTree: ActionTree<State, any> = {
+  async joinChat({commit}, userName: string) {
+    commit('setUser', userName);
+    socket.emit('joinChat', userName);
+    socket.on('receiveMessage', (message: ChatMessage) => {
+      commit('addMessage', message);
+    });
+    socket.on('errorMessage', (message: string) => {
+      commit('setError', message);
+    });
+  },
+  async sendMessage({state}: ActionContext<State, any>, message: ChatMessage) {
+    socket.emit('sendMessage', message);
+  },
+};
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: new State(),
+  getters: getterTree,
+  mutations: mutationTree,
+  actions: actionTree,
+  strict: debug,
 });
