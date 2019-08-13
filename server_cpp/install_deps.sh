0183d2d4193da759d45b18562157529e1cbe3a59
Anders Wind <awia00@gmail.com>
2019-08-13 22:19:40 +0200

    Ensured cpprest works on ubuntu

--- server_cpp/install_deps.sh
+++ server_cpp/install_deps.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt-get install gcc g++ cmake ccache libcpprest-dev
+# cpprest dependencies
+apt-get install -y g++ git libboost-atomic-dev libboost-thread-dev libboost-system-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-random-dev libboost-chrono-dev libboost-serialization-dev libwebsocketpp-dev openssl libssl-dev libcpprest-dev
