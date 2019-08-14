6ee0ee5cc8f310f63b0422dc35753dae26c61649
Anders Wind <awia00@gmail.com>
2019-08-14 22:39:47 +0200

    Removed unused temp component from client

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>P2P</span>
+        <span>WS</span>
         <span class="font-weight-light">Chat</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
